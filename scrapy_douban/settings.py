5f5392fcbf429583b71502091bc79c408d8c9892
sosound <clearputaoshu@163.com>
2023-01-10 15:54:57 +0800

    pages crawl existing problem

--- scrapy_douban/settings.py
+++ scrapy_douban/settings.py
@@ -27,8 +27,8 @@ ROBOTSTXT_OBEY = True
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 
-# DOWNLOAD_DELAY = 3
-# RANDOMIZE_DOWNLOAD_DELAY = True
+DOWNLOAD_DELAY = 3
+RANDOMIZE_DOWNLOAD_DELAY = True
 
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
--- scrapy_douban/spiders/douban.py
+++ scrapy_douban/spiders/douban.py
@@ -1,7 +1,13 @@
 import scrapy
-from scrapy import Selector
+from scrapy import Selector, Request
 
 from scrapy_douban.items import MovieItem
+
+"""
+    该方法可以获取pagination按钮进行多个页面的爬取，但是存在问题。第一页会重复爬取
+一遍。原因是页面切换第一页之外的页面后，第一页的索引Url发生了改变。
+    调整下方start_urls的链接并不能解决问题，原因不明。
+"""
 
 
 class DoubanSpider(scrapy.Spider):
@@ -18,3 +24,8 @@ class DoubanSpider(scrapy.Spider):
             movie_item['rank'] = list_item.css('span.rating_num::text').extract_first()
             movie_item['subject'] = list_item.css('span.inq::text').extract_first()
             yield movie_item
+
+        hrefs_list = sel.css('#content > div > div.article > div.paginator > a::attr(href)')
+        for href in hrefs_list:
+            url = response.urljoin(href.extract())
+            yield Request(url=url)
