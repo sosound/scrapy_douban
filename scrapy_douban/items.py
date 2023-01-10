6fdf8f2ef904fd6b43a9d1fd3f71c71f75bcd8d2
sosound <clearputaoshu@163.com>
2023-01-10 16:09:27 +0800

    pages with detail crawl

--- scrapy_douban/items.py
+++ scrapy_douban/items.py
@@ -10,3 +10,5 @@ class MovieItem(scrapy.Item):
     title = scrapy.Field()
     rank = scrapy.Field()
     subject = scrapy.Field()
+    duration = scrapy.Field()
+    intro = scrapy.Field()
--- scrapy_douban/spiders/douban.py
+++ scrapy_douban/spiders/douban.py
@@ -15,15 +15,26 @@ class DoubanSpider(scrapy.Spider):
     start_urls = ['https://movie.douban.com/top250/']
 
     def start_requests(self):
-        for page in range(11):
+        for page in range(2):
             yield Request(url=f'https://movie.douban.com/top250?start={page*25}&filter=')
 
     def parse(self, response, **kwargs):
         sel = Selector(response)
         list_items = sel.css('#content > div > div.article > ol > li')
         for list_item in list_items:
+            detail_url = list_item.css('div.info > div.hd > a::attr(href)').extract_first()
             movie_item = MovieItem()
             movie_item['title'] = list_item.css('span.title::text').extract_first()
             movie_item['rank'] = list_item.css('span.rating_num::text').extract_first()
             movie_item['subject'] = list_item.css('span.inq::text').extract_first()
-            yield movie_item
+            yield Request(
+                url=detail_url, callback=self.parse_detail,
+                cb_kwargs={'item': movie_item}
+            )
+
+    def parse_detail(self, response, **kwargs):
+        sel = Selector(response)
+        movie_item = kwargs['item']
+        movie_item['duration'] = sel.css('span[property="v:runtime"]::attr(content)').extract()
+        movie_item['intro'] = sel.css('span[property="v:summary"]::text').extract_first()
+        yield movie_item
